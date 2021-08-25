1687f120c33624226b9614c5d0af3cf5c933bbc7
Camille Chang <hong13626>
2021-08-25 20:57:52 +1000

    project 2 init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* weather detail */}
+      <div className="weather-detail">
+        <h2>12 </h2>
+        <h4>Cloudy</h4>
+        <h3 className="city-name">FRANCE</h3>
+        <div>
+          <label>Humidity</label>
+          <label>14%</label>
+        </div>
+        <div>
+          <label>Wind</label>
+          <label>12 K/M</label>
+        </div>
+      </div>
+
+      {/* left part */}
+      <div className="bottomdetail">
+        <div className="leftpart">
+          <div>Twitter Feed</div>
+          <label>ddd</label>
+          <label>ddd</label>
+          <div>Next</div>
+        </div>
+        {/* weekly weather */}
+        <div className="rightpart">
+          <div className="dayWeather">
+            <label>MON</label>
+            <label>picture</label>
+            <label>Raining</label>
+            <label>9'</label>
+          </div>
+          <div className="dayWeather">
+            <label>MON</label>
+            <label>picture</label>
+            <label>Raining</label>
+            <label>9'</label>
+          </div>
+          <div className="dayWeather">
+            <label>MON</label>
+            <label>picture</label>
+            <label>Raining</label>
+            <label>9'</label>
+          </div>
+          <div className="dayWeather">
+            <label>MON</label>
+            <label>picture</label>
+            <label>Raining</label>
+            <label>9'</label>
+          </div>
+          <div className="dayWeather">
+            <label>MON</label>
+            <label>picture</label>
+            <label>Raining</label>
+            <label>9'</label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
