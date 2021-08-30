5633391493c8ef06211f8bb24ecc7a3a925c89ab
Camille Chang <hong13626>
2021-08-30 11:55:56 +1000

    First Edition

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import "./App.css";
 
 import { FaTwitter, FaCircle } from "react-icons/fa";
 import { BsFillCaretRightFill } from "react-icons/bs";
-import { WiRaindrops, WiThunderstorm, WiDaySunny, WiDayCloudy } from "react-icons/wi";
+import {
+  WiRain,
+  WiThunderstorm,
+  WiDaySunny,
+  WiDayCloudy,
+} from "react-icons/wi";
 
 function App() {
   return (
@@ -22,6 +27,7 @@ function App() {
               <h4>HUMIDITY</h4>
               <span>64%</span>
             </div>
+            <span className="hw-seperated-line"></span>
             <div className="wind">
               <h4>WIND</h4>
               <span>12 K/M</span>
@@ -30,69 +36,66 @@ function App() {
         </div>
       </div>
 
-
-
-      {/* left part */}
+      {/* left twitter feed----- */}
       <div className="botom-part">
         <div className="left-part">
           <div className="twitter-feed">
             <FaTwitter className="twitter-logo" />
             <h3>Twitter Feed </h3>
-            <span>#France</span>
+            <span className="cityhashtag">#France</span>
           </div>
 
-          <div className="twitter-feed-words">
-            <FaCircle className="grey-circle" />
-            <p>Don't forget your sunscreen tomorrow! </p>
+          <div className="twitter-news">
+            <div className="twitter-feed-words">
+              <FaCircle className="grey-circle" />
+              <p>Don't forget your sunscreen tomorrow! </p>
+            </div>
+            <div className="twitter-feed-words">
+              <FaCircle className="grey-circle" />{" "}
+              <p>Amazing weather in Paris!</p>
+            </div>
           </div>
-          <div className="twitter-feed-words">
-            <FaCircle className="grey-circle" />   <p>Amazing weather in Paris!</p>
-          </div>
+
           <div className="next-div">
-
             <h4 className="next-text">NEXT</h4>
-            <BsFillCaretRightFill className="twitter-next" />
-
+            <BsFillCaretRightFill className="next-arrow" />
           </div>
-
         </div>
         {/* weekly weather */}
         <div className="right-part">
           <div className="dayWeather">
             <h3>MON</h3>
-            <WiRaindrops className="dayweather-icons dayweather-rain" />
+            <WiRain className="dayweather-icons" />
             <h4>9°</h4>
             <h6>RAINING</h6>
           </div>
           <div className="dayWeather ">
             <h3>TUE</h3>
-            <WiDaySunny className="dayweather-icons dayweather-icons-sunny" />
+            <WiDaySunny className="dayweather-icons" />
             <h4>15°</h4>
             <h6>SUNNY</h6>
           </div>
           <div className="dayWeather  ">
             <h3>WED</h3>
-            <WiDayCloudy className="dayweather-icons dayweather-icons-sunny" />
+            <WiDayCloudy className="dayweather-icons" />
             <h4>11°</h4>
             <h6>CLOUDY</h6>
           </div>
           <div className="dayWeather  ">
             <h3>THU</h3>
-            <WiThunderstorm className="dayweather-icons dayweather-icons-sunny" />
+            <WiThunderstorm className="dayweather-icons" />
             <h4>7°</h4>
             <h6>STORM</h6>
-
           </div>
           <div className="dayWeather">
             <h3>FRI</h3>
-            <WiDaySunny className="dayweather-icons dayweather-icons-sunny" />
+            <WiDaySunny className="dayweather-icons" />
             <h4>18°</h4>
             <h6>SUNNY</h6>
           </div>
         </div>
       </div>
     </div>
-
   );
 }
 
