2e1e757fd117dccc8608c0853afe518c668f7f15
camillechang <camille7710@yahoo.com>
2021-09-08 09:00:25 +1000

    Add searchbar ui and update UI

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
 import "./App.css";
 
 import React, { useState } from "react";
-import { FaTwitter, FaCircle } from "react-icons/fa";
-import { BsSearch, BsFillCaretRightFill } from "react-icons/bs";
 
 import CurrentWeather from "./components/weather/CurrentWeather";
 import DailyWeather from "./components/weather/DailyWeather";
-
 import TwitterFeed from "./components/weather/TwitterFeed";
+import SearchBar from "./components/UI/SearchBar";
 
-function App() {
+const App = (props) => {
 
-  let cityInput = React.createRef();
-  // let cityName = "Canberra";
-  const [cityName, setCityName] = useState("Canberra");
-
-  function searchCityWeather() {
-    let city = cityInput.current.value;
-    console.log("search button clicks, city:", city);
-    if (city !== undefined) {
-      // cityName = city;
-      setCityName(city);
-    }
+  const [city, setCity] = useState("Canberra");
+  const SaveDataHandler = (data) => {
+    // const expendData = data
+    console.log("App--get data from input:-----", data);
+    // console.log(data);
+    // const { cityName } = data.cityName;
+    setCity(data.cityName);
   }
 
   return (
-    <div>
-      <div className="header">
-        <BsSearch />
-        <input type="text" ref={cityInput} placeholder="Search by city name" />
-        <button type="button" onClick={searchCityWeather}>Search</button>
-      </div>
 
+    <div className="weather-main">
+      <SearchBar onSaveTitleData={SaveDataHandler} />
       <section className="container">
-        {/* weather detail */}
-        <CurrentWeather cityName={cityName} />
-        {/* left twitter feed----- */}
+        <CurrentWeather cityName={city} />
+
         <div className="botom-part">
-          <TwitterFeed cityName={cityName} />
-          {/* weekly weather */}
-          <DailyWeather />
-
-
-        </div> </section></div >
+          <TwitterFeed cityName={city} />
+          <DailyWeather cityName={city} />
+        </div>
+      </section>
+    </div>
   );
 }
 
